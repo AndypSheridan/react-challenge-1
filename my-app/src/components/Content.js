6ddef4e7bc62387d7b2b41ad93355c3b2c47c256
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-12 11:23:07 +0000

    Complete challenge 4 and 5

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react'
-import {savedPosts} from "../posts.json"
+import { savedPosts } from "../posts.json"
 import css from "./css/Content.module.css"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
 
-    constructor(props) {
-      super(props)
-    
-      this.state = {}
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoaded: false
     }
+  }
+
+  setTimer() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true
+      })
+    }, 2000)
+  }
+
+  componentDidMount() {
+    this.setTimer()
+  }
 
   render() {
     return (
-      <div className = {css.Content}>
-        <div className = {css.TitleBar}>
-            <h1>My Photos</h1>
+      <div className={css.Content}>
+        <div className={css.TitleBar}>
+          <h1>My Photos</h1>
         </div>
+
         <div className={css.SearchResults}>
-            <PostItem savedPosts = {savedPosts} />
-            </div>
+
+          {this.state.isLoaded ? (
+            <PostItem savedPosts={savedPosts} />
+          ) :
+            (<Loader />)}
+
+        </div>
       </div>
     )
   }
